236c79c3b914a0f003e86044d5e585c1a05216f8
JFlaherty347 <Jflaherty347@gmail.com>
2020-09-30 17:04:23 -0700

    updated function structure to be better utilized from other classes

--- dbTest.cpp
+++ dbTest.cpp
@@ -4,18 +4,20 @@
  *  Created on: Sep 29, 2020
  *      Author: joey
  */
-
+//g++ dbTest.cpp -lsqlite3 -o dbTest.out
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
 #include <string>
 
-int openDatabase(sqlite3 **database)
+sqlite3 *database;
+
+int openDatabase()
 {
-   int databaseConnection = sqlite3_open("users.db", database);
+   int databaseConnection = sqlite3_open("users.db", &database);
 
    if (databaseConnection)
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(*database));
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(database));
    else
       fprintf(stderr, "Opened database successfully\n");
 
@@ -35,8 +37,10 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName)
    return 0;
 }
 
-void createUserTable(sqlite3 *database)
+void createUserTable()
 {
+   openDatabase();
+
    char *errorMessage;  
    char const *table = "CREATE TABLE IF NOT EXISTS USERS(" 
          "ID INT PRIMARY KEY NOT NULL," 
@@ -55,10 +59,13 @@ void createUserTable(sqlite3 *database)
    else
       fprintf(stdout, "Table created successfully!\n");
 
+   sqlite3_close(database);
 }
 
-void insertNewRecord(sqlite3 *database, int id, std::string name, int hours, std::string lastLogTime, int isLoggedIn)
+void insertNewRecord(int id, std::string name, int hours, std::string lastLogTime, int isLoggedIn)
 {
+   openDatabase();
+
    char *errorMessage; 
    char *record = (char *)malloc(sizeof(char) * 128);
    const char *baseCommand = "INSERT INTO USERS (ID,NAME,HOURS,LASTLOGTIME,ISLOGGEDIN) VALUES (%d, '%s', %d, '%s', %d ); ";
@@ -76,10 +83,14 @@ void insertNewRecord(sqlite3 *database, int id, std::string name, int hours, std
    }
    else
       fprintf(stdout, "Record inserted successfully!\n");
+
+   sqlite3_close(database);
 }
 
-void printAllRecords(sqlite3 *database)
+void printAllRecords()
 {
+   openDatabase();
+
    char *errorMessage;  
    char const *command = "SELECT * from USERS";
 
@@ -92,10 +103,14 @@ void printAllRecords(sqlite3 *database)
    }
    else
       fprintf(stdout, "Table printed successfully!\n");
+
+   sqlite3_close(database);
 }
 
-void updateRecord(sqlite3 *database, int id, int hours, std::string lastLogTime, int isLoggedIn)
+void updateRecord(int id, int hours, std::string lastLogTime, int isLoggedIn)
 {
+   openDatabase();
+
    char *errorMessage; 
    char *record = (char *)malloc(sizeof(char) * 128);
    const char *baseCommand = "UPDATE USERS SET " 
@@ -118,10 +133,14 @@ void updateRecord(sqlite3 *database, int id, int hours, std::string lastLogTime,
    }
    else
       fprintf(stdout, "Record updated successfully!\n");
+
+   sqlite3_close(database);
 }
 
-void deleteRecord(sqlite3 *database, int id)
+void deleteRecord(int id)
 {
+   openDatabase();
+
    char *errorMessage; 
    char *record = (char *)malloc(sizeof(char) * 128);
    const char *baseCommand = "DELETE from USERS " 
@@ -139,21 +158,19 @@ void deleteRecord(sqlite3 *database, int id)
    }
    else
       fprintf(stdout, "Record updated successfully!\n");
+
+   sqlite3_close(database);
 }
 
 int main(int argc, char *argv[])
 {
    fprintf(stdout, "Opening and creating a table...\n");
-   sqlite3 *database;
-   openDatabase(&database);
 
-   createUserTable(database);
-   insertNewRecord(database, 0, "Joey", 0, "0000-00-00 00:00:00.000", 0);
-   printAllRecords(database);
-   updateRecord(database, 0, 3, "2020-09-30 15:53:30.000", 1);
-   printAllRecords(database);
-   deleteRecord(database, 0);
-   printAllRecords(database);
-
-   sqlite3_close(database);
+   createUserTable();
+   insertNewRecord(0, "Joey", 0, "0000-00-00 00:00:00.000", 0);
+   printAllRecords();
+   updateRecord(0, 3, "2020-09-30 15:53:30.000", 1);
+   printAllRecords();
+   deleteRecord(0);
+   printAllRecords();
 }
